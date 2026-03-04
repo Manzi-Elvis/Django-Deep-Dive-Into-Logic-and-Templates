b341404afb9f327f3d02f2d15e7d66d33ba1ed80
MANZI RURANGIRWA Elvis <elvismanzi16@gmail.com>
2026-03-04 15:41:17 +0200

    Updated Home View to Use Database

--- core/views.py
+++ core/views.py
@@ -1,14 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .models import Message
+from .forms import MessageForm
+
 
 def home(request):
+    all_messages = Message.objects.all().order_by('-created_at')
+
     context = {
-        "name": "Elvis",
-        "messages": [
-            "Django Templates are powerful",
-            "Logic makes apps dynamic",
-            "Forms make apps interactive"
-        ],
-        "age": 20
+        "messages": all_messages
     }
 
     return render(request, "home.html", context)
+
+
+def submit_message(request):
+    if request.method == "POST":
+        form = MessageForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Message submitted successfully!")
+            return redirect("home")
+    else:
+        form = MessageForm()
+
+    return render(request, "submit_message.html", {"form": form})
